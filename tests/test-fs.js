83e7670aa6146d3ec6015f8025173295f1ea91ec
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-08-23 06:35:31 +0300

    Updated tests

--- tests/test-fs.js
+++ tests/test-fs.js
@@ -26,44 +26,53 @@ describe('fs', function(){
 	});
 
 	it('.rename is callable',     function(){ assert.strictEqual(typeof fs.rename, 'function'); });
-	it.skip('.ftruncate is callable',  function(){ assert.strictEqual(typeof fs.ftruncate, 'function'); });
-	it.skip('.truncate is callable',   function(){ assert.strictEqual(typeof fs.truncate, 'function'); });
-	it.skip('.chown is callable',      function(){ assert.strictEqual(typeof fs.chown, 'function'); });
-	it.skip('.fchown is callable',     function(){ assert.strictEqual(typeof fs.fchown, 'function'); });
-	it.skip('.lchown is callable',     function(){ assert.strictEqual(typeof fs.lchown, 'function'); });
-	it.skip('.chmod is callable',      function(){ assert.strictEqual(typeof fs.chmod, 'function'); });
-	it.skip('.fchmod is callable',     function(){ assert.strictEqual(typeof fs.fchmod, 'function'); });
-	it.skip('.lchmod is callable',     function(){ assert.strictEqual(typeof fs.lchmod, 'function'); });
-	it.skip('.stat is callable',       function(){ assert.strictEqual(typeof fs.stat, 'function'); });
-	it.skip('.lstat is callable',      function(){ assert.strictEqual(typeof fs.lstat, 'function'); });
-	it.skip('.fstat is callable',      function(){ assert.strictEqual(typeof fs.fstat, 'function'); });
-	it.skip('.link is callable',       function(){ assert.strictEqual(typeof fs.link, 'function'); });
-	it.skip('.symlink is callable',    function(){ assert.strictEqual(typeof fs.symlink, 'function'); });
-	it.skip('.readlink is callable',   function(){ assert.strictEqual(typeof fs.readlink, 'function'); });
-	it.skip('.realpath is callable',   function(){ assert.strictEqual(typeof fs.realpath, 'function'); });
+	it('.ftruncate is callable',  function(){ assert.strictEqual(typeof fs.ftruncate, 'function'); });
+	it('.truncate is callable',   function(){ assert.strictEqual(typeof fs.truncate, 'function'); });
+	it('.chown is callable',      function(){ assert.strictEqual(typeof fs.chown, 'function'); });
+	it('.fchown is callable',     function(){ assert.strictEqual(typeof fs.fchown, 'function'); });
+	it('.chmod is callable',      function(){ assert.strictEqual(typeof fs.chmod, 'function'); });
+	it('.fchmod is callable',     function(){ assert.strictEqual(typeof fs.fchmod, 'function'); });
+	it('.stat is callable',       function(){ assert.strictEqual(typeof fs.stat, 'function'); });
+	it('.lstat is callable',      function(){ assert.strictEqual(typeof fs.lstat, 'function'); });
+	it('.fstat is callable',      function(){ assert.strictEqual(typeof fs.fstat, 'function'); });
+	it('.link is callable',       function(){ assert.strictEqual(typeof fs.link, 'function'); });
+	it('.symlink is callable',    function(){ assert.strictEqual(typeof fs.symlink, 'function'); });
+	it('.readlink is callable',   function(){ assert.strictEqual(typeof fs.readlink, 'function'); });
+	it('.realpath is callable',   function(){ assert.strictEqual(typeof fs.realpath, 'function'); });
 	it('.unlink is callable',     function(){ assert.strictEqual(typeof fs.unlink, 'function'); });
-	it.skip('.rmdir is callable',      function(){ assert.strictEqual(typeof fs.rmdir, 'function'); });
+	it('.rmdir is callable',      function(){ assert.strictEqual(typeof fs.rmdir, 'function'); });
 	it('.mkdir is callable',      function(){ assert.strictEqual(typeof fs.mkdir, 'function'); });
 	it('.readdir is callable',    function(){ assert.strictEqual(typeof fs.readdir, 'function'); });
-	it.skip('.close is callable',      function(){ assert.strictEqual(typeof fs.close, 'function'); });
-	it.skip('.open is callable',       function(){ assert.strictEqual(typeof fs.open, 'function'); });
-	it.skip('.utimes is callable',     function(){ assert.strictEqual(typeof fs.utimes, 'function'); });
-	it.skip('.futimes is callable',    function(){ assert.strictEqual(typeof fs.futimes, 'function'); });
-	it.skip('.fsync is callable',      function(){ assert.strictEqual(typeof fs.fsync, 'function'); });
-	it.skip('.write is callable',      function(){ assert.strictEqual(typeof fs.write, 'function'); });
-	it.skip('.read is callable',       function(){ assert.strictEqual(typeof fs.read, 'function'); });
+	it('.close is callable',      function(){ assert.strictEqual(typeof fs.close, 'function'); });
+	it('.open is callable',       function(){ assert.strictEqual(typeof fs.open, 'function'); });
+	it('.utimes is callable',     function(){ assert.strictEqual(typeof fs.utimes, 'function'); });
+	it('.futimes is callable',    function(){ assert.strictEqual(typeof fs.futimes, 'function'); });
+	it('.fsync is callable',      function(){ assert.strictEqual(typeof fs.fsync, 'function'); });
+	it('.write is callable',      function(){ assert.strictEqual(typeof fs.write, 'function'); });
+	it('.read is callable',       function(){ assert.strictEqual(typeof fs.read, 'function'); });
 	it('.readFile is callable',   function(){ assert.strictEqual(typeof fs.readFile, 'function'); });
 	it('.writeFile is callable',  function(){ assert.strictEqual(typeof fs.writeFile, 'function'); });
-	it.skip('.appendFile is callable', function(){ assert.strictEqual(typeof fs.appendFile, 'function'); });
+	it('.appendFile is callable', function(){ assert.strictEqual(typeof fs.appendFile, 'function'); });
 	it('.exists is callable',     function(){ assert.strictEqual(typeof fs.exists, 'function'); });
 
+	// These are only available on Mac OS X
+	it.skip('.lchown is callable',     function(){ assert.strictEqual(typeof fs.lchown, 'function'); });
+	it.skip('.lchmod is callable',     function(){ assert.strictEqual(typeof fs.lchmod, 'function'); });
 
 	describe('fs', function(){
 		
 		var test_dir = __dirname + "/tmp";
 		
 		beforeEach(function(done){
-			fs.mkdir(test_dir, '0700').writeFile(test_dir + '/test1.txt', 'Hello World', {encoding:'utf8', mode:'0644'}).then(function(fs) {
+			fs.exists(test_dir + '/test1.txt').then(function(exists) {
+				return (exists) ? fs.unlink(test_dir + '/test1.txt') : fs;
+			}).$exists(test_dir + '/test2.txt').then(function(exists) {
+				return (exists) ? fs.unlink(test_dir + '/test2.txt') : fs;
+			}).$exists(test_dir + '/test3.txt').then(function(exists) {
+				return (exists) ? fs.unlink(test_dir + '/test3.txt') : fs;
+			}).$exists(test_dir).then(function(exists) {
+				return (exists) ? fs : fs.mkdir(test_dir, '0700');
+			}).$writeFile(test_dir + '/test1.txt', 'Hello World', {encoding:'utf8', mode:'0644'}).then(function(fs) {
 				done();
 			}).fail(function(err) {
 				done(err);
@@ -71,7 +80,15 @@ describe('fs', function(){
 		});
 
 		afterEach(function(done){
-			fs.unlink(test_dir + '/test1.txt').unlink(test_dir).then(function(fs) {
+			fs.exists(test_dir + '/test1.txt').then(function(exists) {
+				return (exists) ? fs.unlink(test_dir + '/test1.txt') : fs;
+			}).$exists(test_dir + '/test2.txt').then(function(exists) {
+				return (exists) ? fs.unlink(test_dir + '/test2.txt') : fs;
+			}).$exists(test_dir + '/test3.txt').then(function(exists) {
+				return (exists) ? fs.unlink(test_dir + '/test3.txt') : fs;
+			}).$exists(test_dir).then(function(exists) {
+				return (exists) ? fs.rmdir(test_dir) : fs;
+			}).then(function(fs) {
 				done();
 			}).fail(function(err) {
 				done(err);
@@ -83,11 +100,19 @@ describe('fs', function(){
 				done();
 			}).fail(function(err) {
 				done(err);
-			}).fin(function() {
-				return fs.rename(test_dir+'/test2.txt', test_dir+'/test1.txt');
 			}).done();
 
 		});
+
+		it('#writeFile() can create tmp/test2.txt', function(done){
+			fs.writeFile(test_dir + '/test2.txt', 'Hello World', {encoding:'utf8', mode:'0644'}).then(function(fs) {
+				done();
+			}).fail(function(err) {
+				done(err);
+			}).done();
+
+		});
+
 	});
 
 });
